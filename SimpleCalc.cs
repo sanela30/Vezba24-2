29338482ad45cd9dba785b475c6a0d666eaa7f21
Sanela Cvijic <sanela26@gmail.com>
2022-03-09 11:46:37 +0100

    first commit

---
+++ SimpleCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vezba24._2
+{
+    internal class SimpleCalc
+    {
+
+        public static double Calc(string operation, double num1, double num2)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return num1+num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                case "/":
+                    return num1 / num2;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
